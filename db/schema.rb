77e99ed42d10775fe3a8c4f32e818787c8b568cb
Chisthoper Mella <chisthoper96@gmail.com>
2021-07-21 17:06:20 -0400

    migration to change column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_204216) do
-
+ActiveRecord::Schema.define(version: 2021_07_21_210020) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
